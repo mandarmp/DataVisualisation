e3d32d47a5a73931f692e0b7d0a8797c40f26b49
MANDAR PATIL <33731455+mandarmp@users.noreply.github.com>
2022-01-16 20:47:45 +0000

    Create errorplot.m

---
+++ Matlab/errorplot.m
@@ -0,0 +1,24 @@
+figure;
+
+hold on;
+mny=mean(repeatedMovementTime); % the y means (assumes columns are observations)
+% err = 2*std(repeatedMovementTime)/sqrt(size(repeatedMovementTime,1));
+err = std(repeatedMovementTime);
+errorbar(x_label_locations(1),mny,err,'o')  
+
+hold off;
+hold on;
+mny=mean(changedMovementTime);                    % the y means (assumes columns are observations)
+% err = 2*std(changedMovementTime)/sqrt(size(changedMovementTime,1));
+err = std(changedMovementTime);
+errorbar(x_label_locations(2),mny,err,'o')  
+hold off;
+set(gca,'Xtick',x_label_locations,'XTickLabel',x_axis_labels)
+ylim([250 500])
+xlim([0 7])
+title('Movement time plot','FontSize',10);
+legend('repeat','switch')
+print('t test for movement time')
+[h_movT,p_movT] = ttest2(repeatedMovementTime,changedMovementTime);
+
+saveas(figure(4),'MovementTime','png')
